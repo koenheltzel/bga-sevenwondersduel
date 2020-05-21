322a10df58e31b7e77341946e331e02345f3b525
Koen Heltzel <koenheltzel@gmail.com>
2020-05-21 21:27:40 +0200

    Building: added age and type properties.

--- modules/Building.php
+++ modules/Building.php
@@ -4,7 +4,14 @@ namespace SWD;
 
 class Building extends Item {
 
-    public $color;
+    public $age;
+    public $type;
     public $chain = null; // coins and or resources
+
+    public function __construct($id, $age, $name, $type) {
+        $this->age = $age;
+        $this->type = $type;
+        parent::__construct($id, $name);
+    }
 
 }
