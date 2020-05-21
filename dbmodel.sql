ba4ecb65fe6f6b386d9045b471f5cfed8a30a8e6
Koen Heltzel <koenheltzel@gmail.com>
2020-05-21 13:57:01 +0200

    Copyright header

--- dbmodel.sql
+++ dbmodel.sql
@@ -1,7 +1,7 @@
 
 -- ------
 -- BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
--- SevenWondersDuel implementation : © <Your name here> <Your email address here>
+-- SevenWondersDuel implementation : © Koen Heltzel <koenheltzel@gmail.com>
 -- 
 -- This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
 -- See http://en.boardgamearena.com/#!doc/Studio for more information.
