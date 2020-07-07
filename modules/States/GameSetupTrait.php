676c2b629e928dcdd46f06e2d9249f2ab2b5b3bb
Koen Heltzel <koenheltzel@gmail.com>
2020-07-07 11:50:09 +0200

    Make the card ids match our material ids

--- modules/States/GameSetupTrait.php
+++ modules/States/GameSetupTrait.php
@@ -20,6 +20,9 @@ trait GameSetupTrait
         $this->wonderDeck->pickCardsForLocation(4, 'deck', 'selection2');
         $this->wonderDeck->shuffle('selection2'); // Ensures we have defined card_location_arg
         $this->wonderDeck->moveAllCardsInLocation('deck', 'box');
+        // Make the card ids match our material ids
+        self::DbQuery( "UPDATE wonder SET card_id = card_type_arg + 1000" );
+        self::DbQuery( "UPDATE wonder SET card_id = card_id - 1000" );
 
         // Set up card piles for the 3 ages.
         // "Return to the box, without looking at them, 3 cards from each Age deck.
@@ -35,6 +38,9 @@ trait GameSetupTrait
         $this->buildingDeck->shuffle( 'age3' );
         // Return the remaining Guilds to the box.
         $this->buildingDeck->moveAllCardsInLocation( 'guilds', 'box');
+        // Make the card ids match our material ids
+        self::DbQuery( "UPDATE building SET card_id = card_type_arg + 1000" );
+        self::DbQuery( "UPDATE building SET card_id = card_id - 1000" );
 
         // Set up progress tokens
         $this->progressTokenDeck->createCards(Material::get()->progressTokens->getDeckCards());
@@ -43,6 +49,9 @@ trait GameSetupTrait
         $this->progressTokenDeck->shuffle('board'); // Ensures we have defined card_location_arg
         // Return the remaining Progress Tokens to the box.
         $this->progressTokenDeck->moveAllCardsInLocation('deck', 'box');
+        // Make the card ids match our material ids
+        self::DbQuery( "UPDATE progress_token SET card_id = card_type_arg + 1000" );
+        self::DbQuery( "UPDATE progress_token SET card_id = card_id - 1000" );
 
         // TODO: Remove, this will be done by player interaction:
 //        $playerIds = array_keys($players);
