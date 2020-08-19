12c7de9adc0a685ddaf20968ff289d00671380e7
Koen Heltzel <koenheltzel@gmail.com>
2020-08-19 12:11:05 +0200

    Definition of statistics

--- stats.inc.php
+++ stats.inc.php
@@ -47,49 +47,213 @@
     
 */
 
-$stats_type = array(
+$stats_type = [
 
     // Statistics global to table
-    "table" => array(
+    "table" => [
 
-        "turns_number" => array("id"=> 10,
-                    "name" => totranslate("Number of turns"),
-                    "type" => "int" ),
+        "turns_number" => [
+            "id" => 10,
+            "name" => totranslate("Number of turns"),
+            "type" => "int"
+        ],
+        "civilian_victory" => [
+            "id" => 20,
+            "name" => totranslate("Civilian Victory"),
+            "type" => "int"
+        ],
+        "scientific_supremacy" => [
+            "id" => 30,
+            "name" => totranslate("Scientific Supremacy"),
+            "type" => "int"
+        ],
+        "military_supremacy" => [
+            "id" => 40,
+            "name" => totranslate("Military Supremacy"),
+            "type" => "int"
+        ],
 
-/*
-        Examples:
+        /*
+                Examples:
 
 
-        "table_teststat1" => array(   "id"=> 10,
-                                "name" => totranslate("table test stat 1"), 
-                                "type" => "int" ),
-                                
-        "table_teststat2" => array(   "id"=> 11,
-                                "name" => totranslate("table test stat 2"), 
-                                "type" => "float" )
-*/  
-    ),
-    
+                "table_teststat1" => array(   "id"=> 10,
+                                        "name" => totranslate("table test stat 1"),
+                                        "type" => "int" ),
+
+                "table_teststat2" => array(   "id"=> 11,
+                                        "name" => totranslate("table test stat 2"),
+                                        "type" => "float" )
+        */
+    ],
+
     // Statistics existing for each player
-    "player" => array(
+    "player" => [
 
-        "turns_number" => array("id"=> 10,
-                    "name" => totranslate("Number of turns"),
-                    "type" => "int" ),
-    
-/*
-        Examples:    
-        
-        
-        "player_teststat1" => array(   "id"=> 10,
-                                "name" => totranslate("player test stat 1"), 
-                                "type" => "int" ),
-                                
-        "player_teststat2" => array(   "id"=> 11,
-                                "name" => totranslate("player test stat 2"), 
-                                "type" => "float" )
+        "turns_number" => [
+            "id" => 10,
+            "name" => totranslate("Number of turns"),
+            "type" => "int"
+        ],
+        "civilian_victory" => [
+            "id" => 20,
+            "name" => totranslate("Civilian Victory"),
+            "type" => "int"
+        ],
+        "scientific_supremacy" => [
+            "id" => 30,
+            "name" => totranslate("Scientific Supremacy"),
+            "type" => "int"
+        ],
+        "military_supremacy" => [
+            "id" => 40,
+            "name" => totranslate("Military Supremacy"),
+            "type" => "int"
+        ],
+        "victory_points" => [
+            "id" => 50,
+            "name" => totranslate("Victory Points"),
+            "type" => "int"
+        ],
+        "vp_blue" => [
+            "id" => 60,
+            "name" => totranslate("VP from blue cards"),
+            "type" => "int"
+        ],
+        "vp_green" => [
+            "id" => 70,
+            "name" => totranslate("VP from green cards"),
+            "type" => "int"
+        ],
+        "vp_yellow" => [
+            "id" => 80,
+            "name" => totranslate("VP from yellow cards"),
+            "type" => "int"
+        ],
+        "vp_purple" => [
+            "id" => 90,
+            "name" => totranslate("VP from purple cards"),
+            "type" => "int"
+        ],
+        "vp_wonders" => [
+            "id" => 100,
+            "name" => totranslate("VP from Wonders"),
+            "type" => "int"
+        ],
+        "vp_progress_tokens" => [
+            "id" => 110,
+            "name" => totranslate("VP from Progress Tokens"),
+            "type" => "int"
+        ],
+        "vp_coins" => [
+            "id" => 120,
+            "name" => totranslate("VP from Coins"),
+            "type" => "int"
+        ],
+        "vp_military" => [
+            "id" => 130,
+            "name" => totranslate("VP from Conflict Pawn position"),
+            "type" => "int"
+        ],
+        "brown_cards" => [
+            "id" => 140,
+            "name" => totranslate("Brown cards"),
+            "type" => "int"
+        ],
+        "gray_cards" => [
+            "id" => 150,
+            "name" => totranslate("Gray cards"),
+            "type" => "int"
+        ],
+        "yellow_cards" => [
+            "id" => 160,
+            "name" => totranslate("Yellow cards"),
+            "type" => "int"
+        ],
+        "red_cards" => [
+            "id" => 170,
+            "name" => totranslate("Red cards"),
+            "type" => "int"
+        ],
+        "blue_cards" => [
+            "id" => 180,
+            "name" => totranslate("Blue cards"),
+            "type" => "int"
+        ],
+        "green_cards" => [
+            "id" => 190,
+            "name" => totranslate("Green cards"),
+            "type" => "int"
+        ],
+        "purple_cards" => [
+            "id" => 200,
+            "name" => totranslate("Purple cards"),
+            "type" => "int"
+        ],
+        "wonders_constructed" => [
+            "id" => 210,
+            "name" => totranslate("Wonders constructed"),
+            "type" => "int"
+        ],
+        "progress_tokens" => [
+            "id" => 220,
+            "name" => totranslate("Progress Tokens"),
+            "type" => "int"
+        ],
+        "shields" => [
+            "id" => 230,
+            "name" => totranslate("Shields (Conflict Pawn steps)"),
+            "type" => "int"
+        ],
+        "science_symbols" => [
+            "id" => 240,
+            "name" => totranslate("Science symbols (unique)"),
+            "type" => "int"
+        ],
+        "extra_turns" => [
+            "id" => 250,
+            "name" => totranslate("Extra turns"),
+            "type" => "int"
+        ],
+        "discarded_cards" => [
+            "id" => 260,
+            "name" => totranslate("Discarded cards"),
+            "type" => "int"
+        ],
+        "chained_constructions" => [
+            "id" => 270,
+            "name" => totranslate("Chained constructions"),
+            "type" => "int"
+        ],
 
-*/    
-    )
+        /*
+                Examples:
 
-);
+
+                "player_teststat1" => array(   "id"=> 10,
+                                        "name" => totranslate("player test stat 1"),
+                                        "type" => "int" ),
+
+                "player_teststat2" => array(   "id"=> 11,
+                                        "name" => totranslate("player test stat 2"),
+                                        "type" => "float" )
+
+        */
+    ],
+
+    "value_labels" => [
+        15 => [ // Civilian Victory
+            0 => totranslate("No"),
+            1 => totranslate("Yes"),
+        ],
+        20 => [ // Scientific Supremacy
+            0 => totranslate("No"),
+            1 => totranslate("Yes"),
+        ],
+        25 => [ // Military Supremacy
+            0 => totranslate("No"),
+            1 => totranslate("Yes"),
+        ],
+    ],
+
+];
