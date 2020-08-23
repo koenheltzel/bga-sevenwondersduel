f42e5711bf650fa0c650f8f56d70985e4f61eae0
Koen Heltzel <koenheltzel@gmail.com>
2020-08-23 14:14:09 +0200

    Building.php made getScoreCategory public

--- modules/php/Building.php
+++ modules/php/Building.php
@@ -283,7 +283,7 @@ class Building extends Item {
                 return self::_('Purple');
         }
     }
-    protected function getScoreCategory() {
+    public function getScoreCategory() {
         return strtolower($this->type);
     }
 
