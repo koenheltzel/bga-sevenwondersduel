3d27b93de8e1d62860f67c959cb0858a5292b373
Koen Heltzel <koenheltzel@gmail.com>
2020-08-26 09:36:51 +0200

    Updated text

--- sevenwondersduel.view.php
+++ sevenwondersduel.view.php
@@ -54,7 +54,7 @@ class view_sevenwondersduel_sevenwondersduel extends game_view
         $this->tpl['AUTOMATIC_LAYOUT_DESCRIPTION'] = self::_("Choose game layout automatically based on browser window ratio (recommended).");
         $this->tpl['LAYOUT'] = self::_("Layout");
         $this->tpl['AUTOMATIC_SCALING'] = self::_("Automatic scaling");
-        $this->tpl['AUTOMATIC_SCALING_DESCRIPTION'] = self::_("Choose game scale automatically based on browser window size and current game layout & situation (recommended).");
+        $this->tpl['AUTOMATIC_SCALING_DESCRIPTION'] = self::_("Choose game scale automatically based on browser window size and current game layout & situation (highly recommended).");
         $this->tpl['SCALE'] = self::_("Scale");
         $this->tpl['AGE'] = self::_("Age");
         $this->tpl['GUILDS'] = self::_("Guilds");
