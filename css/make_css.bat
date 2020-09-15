c7f5cdf9df2fd0810b099a4a348c14b0942db151
Koen Heltzel <koenheltzel@gmail.com>
2020-09-15 15:14:15 +0200

    Tweaked make_css.bat, would quit after the "npx postcss" call: https://github.com/npm/npm/issues/2938

--- css/make_css.bat
+++ css/make_css.bat
@@ -1,6 +1,5 @@
 cd css
 type *.css > ../sevenwondersduel_source.css
 cd..
-npx postcss sevenwondersduel_source.css --use autoprefixer --output sevenwondersduel.css --no-map
+call npx postcss sevenwondersduel_source.css --use autoprefixer --output sevenwondersduel.css --no-map
 del sevenwondersduel_source.css
-pause
