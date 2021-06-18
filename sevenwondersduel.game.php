da43b153bafb48815cd6a1df4ab41906fb3b3929
Koen Heltzel <koenheltzel@gmail.com>
2021-06-18 17:51:26 +0200

    Create Pantheon tables if they don't exist yet (for existing games)

--- sevenwondersduel.game.php
+++ sevenwondersduel.game.php
@@ -1007,6 +1007,44 @@ class SevenWondersDuel extends Table
             $sql = "UPDATE `DBPREFIX_progress_token` SET card_location = 'box' WHERE card_location = 'wonder6'";
             self::applyDbUpgradeToAllDB($sql);
         }
+        if ($from_version <= 2106181733) {
+            $sql = "
+                CREATE TABLE IF NOT EXISTS `divinity` (
+                    `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                    `card_type` varchar(16) NOT NULL,
+                    `card_type_arg` int(11) NOT NULL,
+                    `card_location` varchar(16) NOT NULL,
+                    `card_location_arg` int(11) NOT NULL,
+                    PRIMARY KEY (`card_id`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1
+            ";
+            self::applyDbUpgradeToAllDB($sql);
+
+            $sql = "
+                CREATE TABLE IF NOT EXISTS `mythology_token` (
+                    `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                    `card_type` varchar(16) NOT NULL,
+                    `card_type_arg` int(11) NOT NULL,
+                    `card_location` varchar(16) NOT NULL,
+                    `card_location_arg` int(11) NOT NULL,
+                    PRIMARY KEY (`card_id`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1
+            ";
+            self::applyDbUpgradeToAllDB($sql);
+
+            $sql = "
+                CREATE TABLE IF NOT EXISTS `offering_token` (
+                    `card_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                    `card_type` varchar(16) NOT NULL,
+                    `card_type_arg` int(11) NOT NULL,
+                    `card_location` varchar(16) NOT NULL,
+                    `card_location_arg` int(11) NOT NULL,
+                    PRIMARY KEY (`card_id`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+            ";
+            self::applyDbUpgradeToAllDB($sql);
+        }
+
 
     }
 
